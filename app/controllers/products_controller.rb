46e99495f029c165dedff26e8697c2262135479f
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2023-09-19 23:46:43 -0300

    cambios

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,24 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+  def create
+    @product = Product.new(product_params)
+    pp @product
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
 end
+
+
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,6 +11,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select '.title', 'Cinnamon Roll Lamp'
     assert_select '.description', 'Moon led lamp'
-    assert_select '.price', 300
+    assert_select '.price', '300'
+  end
+
+  test 'render a new product form' do
+  get new_product_path
+    assert_response :success
+    assert_select 'form'
+
+    end
+  test 'allow create a new product' do
+    post products_path, params: { product: { title: 'Le Wagon Cap', description: 'Keep the sun out of your eyes', price: 25 } }
+    assert_redirected_to products_path
   end
 end
