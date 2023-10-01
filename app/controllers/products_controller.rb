542a11443fef0d1a9f5c434286783b80a41163b2
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2023-10-01 02:21:22 -0300

    create user validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,6 +43,10 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo, :category_id)
   end
+  def products_params_index
+    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by)
+
+  end
   def product
     @product ||= Product.find(params[:id])
   end
