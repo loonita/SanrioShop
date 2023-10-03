be06021763d2409deac5b4a679c92679bda652fe
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2023-10-03 01:29:19 -0300

    login

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -2,10 +2,14 @@ class Authentication::UsersController < ApplicationController
   def new
     @user = User.new
   end
+  {
+    user_id: 1
+  }
   def create
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to products_path, notice: t('.created')
     else
       render :new, status: :unprocessable_entity
