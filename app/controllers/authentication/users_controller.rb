9f8a32b481b403ddde75453c98fcfa46d1f176fb
Teresa Vidal <teresa.vidal2001@alumnos.ubiobio.cl>
2023-10-05 01:05:46 -0300

    protect login

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -1,10 +1,10 @@
 class Authentication::UsersController < ApplicationController
+  skip_before_action :protect_pages
+
   def new
     @user = User.new
   end
-  {
-    user_id: 1
-  }
+
   def create
     @user = User.new(user_params)
 
@@ -15,7 +15,9 @@ class Authentication::UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   private
+
   def user_params
     params.require(:user).permit(:email, :username, :password)
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -18,5 +18,6 @@ class Product < ApplicationRecord
   validates :price, presence: true
 
   belongs_to :category
+  belongs_to :user, default: -> { Current.user }
 
 end
